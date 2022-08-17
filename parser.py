94f69987cc13760757fa304d3cbea45a7474b84a
myrzaiymK <92356144+myrzaiymK@users.noreply.github.com>
2022-08-18 00:45:58 +0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,8 +2,6 @@ import requests
 import json
 import datetime
 
-with open('dannye.json', 'a') as file:
-    file.write(a)
 
 datetime = str(datetime.datetime.now()) 
 JSON = 'http://newsline.kg/getNews.php?limit=30&last_dt=' + datetime
@@ -11,3 +9,6 @@ get = requests.get(JSON)
 data = get.json()
 a = json.dumps(data, indent=4, sort_keys=True)
 # print(a)
+
+with open('dannye.json', 'a') as file:
+    file.write(a)
